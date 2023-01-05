5a02a27108da086e981e2d9f16085d2df3245b35
ChristineAshraf <Christine1907011@miuegypt.edu.eg>
2023-01-06 01:55:32 +0200

    ui update and validation

--- flutter_application_1/lib/screens/admin/addTrip.dart
+++ flutter_application_1/lib/screens/admin/addTrip.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '/widgets/date&time picker.dart';
 import 'package:go_router/go_router.dart';
+import 'package:flutter_application_1/widgets/custom_TextField.dart';
 
 class AddTrip extends StatefulWidget {
   static String id = 'AddTrip';
@@ -10,81 +11,13 @@ class AddTrip extends StatefulWidget {
 }
 
 class _AddTripState extends State<AddTrip> {
-  // String _name, _price, _description, _startdate, _enddate;
 
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     //  resizeToAvoidBottomPadding: false,
-
-  //     body: Form(
-  //       child: Padding(
-  //         padding: const EdgeInsets.symmetric(horizontal: 30 , vertical: 20),
-  //         child: ListView(
-  //           children: <Widget>[
-  //             TextField("Trip_Name" , "Name" ,"Can't be empty"),
-
-  //             SizedBox(
-  //               height: 20,
-  //             ),
-  //             TextField("Trip_ID" , "ID" ,"Can't be empty"),
-  //             SizedBox(
-  //               height: 20,
-  //             ),
-  //             TextField("Trip_Discription" , "Discription" ,"Can't be empty"),
-  //             SizedBox(
-  //               height: 20,
-  //             ),
-  //             TextField("Trip_Price" , "Price" ,"Can't be empty"),
-  //             SizedBox(
-  //               height: 20,
-  //             ),
-  //             Row(
-  //                 children: [
-  //                   Text(
-  //                         'Start Date : ',
-  //                         style: TextStyle( fontSize: 20),
-  //                       ),
-
-  //                   DateTimePicker(),
-  //                 ]
-  //             ),
-  //              SizedBox(
-  //               height: 20,
-  //             ),
-  //             Row(
-  //                 children: [
-  //                   Text(
-  //                         'End Date : ',
-  //                         style: TextStyle( fontSize: 20),
-  //                       ),
-
-  //                   DateTimePicker(),
-  //                 ]
-  //             ),
-  //             SizedBox(
-  //                 height: 10,
-  //               ),
-  //               // RaisedButton(
-  //               //   onPressed: () {
-  //               //     if (_globalKey.currentState.validate()) {
-  //               //       _globalKey.currentState.save();
-
-  //               //       _store.addTrip(Product(
-  //               //           pName: _name,
-  //               //           pPrice: _price,
-  //               //           pDescription: _description,
-  //               //           pLocation: _imageLocation,
-  //               //           pCategory: _category));
-  //               //     }
-  //               //   },
-  //               //   child: Text('Add Product'),
-  //               // )
-  //         ]),
-  //       ),
-  //     ),
-  //   );
-  // }
+   String _tripName='' ;
+   String _description='' ;
+   String _price='' ; 
+   String _tripId ='' ;
+  
+  final GlobalKey<FormState> _globalKey = GlobalKey<FormState>(); 
 
   @override
   Widget build(BuildContext context) {
@@ -97,12 +30,12 @@ class _AddTripState extends State<AddTrip> {
               Column(
                 children: [
                   Container(
-                      width: 500,
+                      width: 400,
                       height: 900,
                       // color: Colors.green,
                       decoration: const BoxDecoration(
                         image: DecorationImage(
-                            image: AssetImage("assets/travel_plane_corona.png"),
+                            image: AssetImage("assets/t.jpg"),
                             fit: BoxFit.cover),
                       )),
                   // Container(
@@ -115,7 +48,7 @@ class _AddTripState extends State<AddTrip> {
               Container(
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.all(Radius.circular(20)),
-                  color: Color.fromARGB(255, 238, 154, 111),
+                  color: Color.fromARGB(255, 98, 191, 238),
                 ),
                 width: 330,
                 height: 510,
@@ -128,7 +61,7 @@ class _AddTripState extends State<AddTrip> {
                       style: TextStyle(
                           fontSize: 40,
                           fontWeight: FontWeight.w900,
-                          color: Color.fromARGB(255, 176, 221, 242)),
+                          color: Color.fromARGB(255, 252, 253, 254)),
                     ),
                     // Text(
                     //   'New Trip',
@@ -148,78 +81,120 @@ class _AddTripState extends State<AddTrip> {
                   child: Container(
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.all(Radius.circular(20)),
-                      color: Color.fromARGB(255, 240, 206, 190),
+                      color: Color.fromARGB(255, 252, 250, 250),
                     ),
                     width: 330,
                     height: 450,
                     margin: EdgeInsets.only(bottom: 30),
-                    child: ListView(
-                      // mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        TextField("Trip_Name", "Name", "Can't be empty"),
-
-                        SizedBox(
-                          height: 5,
-                        ),
-                        TextField("Trip_ID", "ID", "Can't be empty"),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        TextField("Trip_Discription", "Discription",
-                            "Can't be empty"),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        TextField("Trip_Price", "Price", "Can't be empty"),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        Row(children: [
-                          Text(
-                            'Start Date : ',
-                            style: TextStyle(fontSize: 20),
+                    child: Form(
+                      key: _globalKey,
+                      child: ListView(
+                        // mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          CustomTextField(
+                            // lable: 'Trip Name',
+                            hint: 'Trip Name',
+                            icon: Icons.hotel ,
+                            onClick: (value) {
+                              _tripName = value!;
+                            },
                           ),
-                          DateTimePicker(),
-                        ]),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        Row(children: [
-                          Text(
-                            'End Date : ',
-                            style: TextStyle(fontSize: 20),
+                          
+                    
+                          SizedBox(
+                            height: 5,
                           ),
-                          DateTimePicker(),
-                        ]),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        // RaisedButton(
-                        //   onPressed: () {
-                        //     if (_globalKey.currentState.validate()) {
-                        //       _globalKey.currentState.save();
+                          CustomTextField(
+                            // lable: 'Trip Name',
+                            hint: 'Trip ID',
+                            icon: Icons.accessibility,
+                            onClick: (value) {
+                              _tripId = value!;
+                            },
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          CustomTextField(
+                            // lable: 'Trip Name',
+                            hint: 'Trip Description',
+                            icon: Icons.description  ,
+                            onClick: (value) {
+                              _description = value!;
+                            },
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          CustomTextField(
+                            // lable: 'Trip Name',
+                            hint: 'Trip Price',
+                            icon: Icons.price_check_outlined ,
+                            onClick: (value) {
+                              _price = value!;
+                            },
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Row(children: [
+                            Text(
+                              'Start Date : ',
+                              style: TextStyle(fontSize: 20),
+                              
+                            ),
+                            DateTimePicker(),
+                          ]),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Row(children: [
+                            Text(
+                              'End Date : ',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            DateTimePicker(),
+                          ]),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          // RaisedButton(
+                          //   onPressed: () {
+                          //     if (_globalKey.currentState.validate()) {
+                          //       _globalKey.currentState.save();
+                    
+                          //       _store.addTrip(Product(
+                          //           pName: _name,
+                          //           pPrice: _price,
+                          //           pDescription: _description,
+                          //           pLocation: _imageLocation,
+                          //           pCategory: _category));
+                          //     }
+                          //   },
+                          //   child: Text('Add Product'),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              TextButton(
+                                  onPressed: () {
+                                    context.go('/AdminHome');
+                                  },
+                                  child: Text('Back')),
 
-                        //       _store.addTrip(Product(
-                        //           pName: _name,
-                        //           pPrice: _price,
-                        //           pDescription: _description,
-                        //           pLocation: _imageLocation,
-                        //           pCategory: _category));
-                        //     }
-                        //   },
-                        //   child: Text('Add Product'),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            TextButton(
-                                onPressed: () {
-                                  context.go('/AdminHome');
+                              TextButton(
+                                onPressed: (){
+                                   if (_globalKey.currentState!.validate()) {
+                                         _globalKey.currentState!.reset();
+
+                                    }
                                 },
-                                child: Text('Back')),
-                            TextButton(onPressed: () {}, child: Text('Add')),
-                          ],
-                        )
-                      ],
+                                child: Text('Add') 
+                                       
+                              ),
+                            ]
+                          )
+                        ],
+                      ),
                     ),
                   )),
             ],
@@ -228,34 +203,4 @@ class _AddTripState extends State<AddTrip> {
       ),
     );
   }
-}
-
-Widget TextField(label, hint, helper) {
-  return Padding(
-    padding: const EdgeInsets.all(10.0),
-    child: TextFormField(
-      // maxLines: 5,
-      decoration: InputDecoration(
-        contentPadding:
-            const EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
-        border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(15.0),
-            borderSide: BorderSide(
-              color: Colors.teal,
-            )),
-        focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(
-          color: Colors.cyan,
-          width: 2,
-        )),
-        prefixIcon: Icon(
-          Icons.flight,
-          color: Colors.cyan,
-        ),
-        labelText: label,
-        hintText: hint,
-        helperText: helper,
-      ),
-    ),
-  );
 }
---
+++ flutter_application_1/lib/widgets/custom_TextField.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+
+  final String hint;
+  // final String lable;, required this.lable
+
+  final IconData icon;
+final FormFieldSetter<String>? onClick;
+
+
+  String ?_errorMessage(String str)
+  {
+    switch(hint)
+    {
+      case 'Trip Name' :return 'Trip name is empty';
+      case 'Trip Description' :return 'Trip Description is empty';
+      case 'Trip Price' :return 'Trip Price is empty';
+      case 'Trip ID' :return 'Trip name is ID';
+    }
+  }
+  
+  CustomTextField(
+      { required this.icon,required this.hint, required this.onClick});
+  @override
+  Widget build(BuildContext context) {
+   
+  return Padding(
+
+    padding: const EdgeInsets.all(10.0),
+    child: TextFormField(
+      // maxLines: 5,
+      validator:(value) {
+        if(value!.isEmpty){
+          return _errorMessage(hint);
+        }
+        
+      } ,
+      onSaved: onClick,
+      decoration: InputDecoration(
+          // labelText: lable ,
+          hintText: hint,
+          prefixIcon: Icon(
+            icon,
+            color: Color.fromARGB(255, 244, 240, 240),
+          ),
+          filled: true,
+          fillColor: Color.fromARGB(255, 52, 169, 228),
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: BorderSide(color: Color.fromARGB(255, 19, 37, 233))),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: BorderSide(color: Color.fromARGB(255, 245, 225, 153))),
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: BorderSide(color: Color.fromARGB(255, 198, 118, 43))),
+        ),
+      ),
+
+  );
+}
+
+}
