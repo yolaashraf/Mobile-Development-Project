315f6305b4cba97ce1bfe61e0a18824e8892b6cd
youmna18 <youmna1909121@miuegypt.edu.eg>
2023-01-21 08:28:29 +0200

    39th

---
+++ lib/screens/admin/manageProducts.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ManageProducts extends StatelessWidget {
+  static String id='ManageProducts';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+            body: Container(),
+    );
+  }
+}
